49a4e2e6ffd1ccdda9ef28c61f733dc409f1ed58
Reid Paape <wrpaape@gmail.com>
2015-05-11 16:18:50 -0500

    hard mode

--- profile-info.rb
+++ profile-info.rb
@@ -23,31 +23,33 @@ residence = Hash.new
 puts "----------------"
 puts "--Profile Info--"
 puts "----------------"
+# read profile after data entry?
+print "read profile after data entry (y/n)? #{prompt}"
+read_profile = gets.chomp.downcase
 
 print "What is your first name? #{prompt}"
-name[0] = gets.chomp
+name[0] = gets.chomp.downcase.capitalize
 print "Middle name? #{prompt}"
-name[1] = gets.chomp
+name[1] = gets.chomp.downcase.capitalize
 if name[1] == ""
   name[1] = "(N/A)"
 end
 print "Last name? #{prompt}"
-name[2] = gets.chomp
+name[2] = gets.chomp.downcase.capitalize
 
 print "What year were you born in (YYYY)? #{prompt}"
 dob[0] = gets.chomp
 print "Month (MM)? #{prompt}"
-dob[1] = gets.chomp
-dob[1] = dob[1].to_i - 1
+dob[1] = gets.chomp.to_i - 1
 print "Day (DD)? #{prompt}"
 dob[2] = gets.chomp
 
 print "What State do you live in? #{prompt}"
-residence["state"] = gets.chomp
+residence["state"] = gets.chomp.downcase.capitalize
 print "City? #{prompt}"
-residence["city"] = gets.chomp
+residence["city"] = gets.chomp.downcase.capitalize
 print "Zipcode? #{prompt}"
-residence["zip"] = gets.chomp
+residence["zip"] = gets.chomp.downcase.capitalize
 
 profile = {
   name: name,
@@ -56,8 +58,14 @@ profile = {
 }
 
 #print profile
+if read_profile == "y"
 puts "----------------"
-puts "Name:__#{profile[:name][0]}_#{profile[:name][1]}_#{profile[:name][2]}"
-puts "Date of Birth:__#{month_name[profile[:dob][1]]}_\
-                      #{profile[:dob][0]},_\
-                      #{profile[:dob][3]}"
+puts "Name:_______________#{profile[:name][0]}_#{profile[:name][1]}_#{profile[:name][2]}"
+puts "Date of Birth:______#{month_name[profile[:dob][1]]}_" + \
+                         "#{profile[:dob][2]},_" + \
+                         "#{profile[:dob][0]}"
+puts "Current Residence:__#{profile[:residence]["city"]},_" + \
+                         "#{profile[:residence]["state"]}_" + \
+                         "#{profile[:residence]["zip"]}"
+puts "----------------"
+end
